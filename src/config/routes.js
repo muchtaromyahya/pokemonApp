cddd7ec9c648fef32ca95121b0ae14e83d58b5bf
muchtaromyahya <muchtaromyahya@student.telkomuniversity.ac.id>
2021-11-08 19:22:12 +0700

    build bundle

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,26 +1,6 @@
-import { Home, Classes, Comments, Posts, Tasks } from '../pages';
+import { Home } from '../pages';
 
 const routes = [
-  {
-    path: '/tasks',
-    component: Tasks,
-    isPublic: true,
-  },
-  {
-    path: '/post',
-    component: Posts,
-    isPublic: true,
-  },
-  {
-    path: '/comments',
-    component: Comments,
-    isPublic: true,
-  },
-  {
-    path: '/classes',
-    component: Classes,
-    isPublic: true,
-  },
   {
     path: '/home',
     component: Home,
